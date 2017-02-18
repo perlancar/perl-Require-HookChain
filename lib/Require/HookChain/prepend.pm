299e61bd4de8b5e7df497e7319cad0ca6ce6c14c
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-02-18 20:15:49 +0700

    Initial commit

---
+++ lib/Require/HookChain/prepend.pm
@@ -0,0 +1,40 @@
+package Require::HookChain::prepend;
+
+# DATE
+# VERSION
+
+sub new {
+    my ($class, $preamble) = @_;
+    bless { preamble => $preamble }, $class;
+}
+
+sub Require::HookChain::prepend::INC {
+    my ($self, $r) = @_;
+
+    # safety, in case we are not called by Require::HookChain
+    return () unless ref $r;
+
+    my $src = $r->src;
+    return unless defined $src;
+    $src = "$self->{preamble};\n$src";
+    $r->src($src);
+}
+
+1;
+# ABSTRACT: Prepend source code to module
+
+=head1 SYNOPSIS
+
+ use Require::HookChain prepend => 'use strict';
+
+The above has a similar effect to:
+
+ use everywhere 'strict';
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Require::HookChain>
