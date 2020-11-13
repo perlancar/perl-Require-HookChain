ec30f2cbede330cb47d3b0b564f5421dcdc15f1b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-11-13 09:17:38 +0700

    Add short alias for Require::HookChain: RHC

---
+++ lib/RHC.pm
@@ -0,0 +1,23 @@
+package RHC;
+use alias::module 'Require::HookChain';
+
+1;
+# ABSTRACT: Short alias for Require::HookChain
+
+=head1 SYNOPSIS
+
+On the command-line:
+
+ # add 'use strict' to all loaded modules
+ % perl -MRHC=munge::prepend,'use strict' ...
+
+
+=head1 DESCRIPTION
+
+This is a short alias for L<Require::HookChain> for less typing on the
+command-line.
+
+
+=head1 SEE ALSO
+
+L<Require::HookChain>
